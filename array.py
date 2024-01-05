3b67e1b52a3ee4086a61292ccbda7f5b81a28c35
pradeepSharmadev <amarahipradeep7319@gmail.com>
2024-01-05 23:39:09 +0530

    first change

---
+++ array.py
@@ -0,0 +1,9 @@
+#WelCome!Pradeep
+
+
+# Itrate each item of an array
+arr = [1,4,6,23,66,78,43]
+
+for i in range(len(arr)):
+    print(f"This is item no {i+1} and value of item is {arr[i]}")
+    print("Next")
