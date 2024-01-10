23ace9bc8740b4da6d48278649853adad63d5606
pradeepSharmadev <amarahipradeep7319@gmail.com>
2024-01-10 23:45:35 +0530

    class is updated

--- pythonChallanges/classe.py
+++ pythonChallanges/classe.py
@@ -1,11 +1,19 @@
+'''classes is the blueprint of real world objects 
+as objects have properties(variable) and methods(functions)
+1st create blueprint of object then create as many as required objects
+'''
+
 class student:
     def __init__(self,name):
-        self.name = name
+        self.name = name # initialising variable
     
     def welcome(self):
-        mms = f"welcome {self.name}"
-        return mms
-
+        mmss = f"{self.name}" #not visible outside method
+        self.mms = f"welcome {self.name}" #acceseble anyware whithin class
+        return self.mms
+    def check(self):
+        return True
+    
 # main
 user = "Pradeep kumar sharma"
 obj = student(user)
@@ -13,3 +21,10 @@ print(obj.name)
 result = obj.welcome()
 print(result)
 print("Know we are intracting with \n class based programming",obj.welcome())
+
+obj2 = student("Rahul")
+print(obj2.name)
+
+print(obj2.check())
+
+
